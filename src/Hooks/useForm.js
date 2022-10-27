616826c1a29c99271e05f64f9c6aa0f9cd8415ec
tiagot1 <tiagotorres.dev@gmail.com>
2022-10-27 20:36:01 -0300

    validacao de email e senha

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const validacao = {
+  regex:
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  message: 'Preencha um Email valido',
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return false;
+    if (value.length === 0) {
+      setError('preencha um valor');
+      return false;
+    } else if (validacao[type] && !validacao[type].regex.test(value)) {
+      setError(validacao[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onblur: () => validate(value),
+  };
+};
+
+export default useForm;
--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ name, label, type }) => {
+const Input = ({ name, label, type, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
-        <input type={type} id={name} name={name} className={styles.input} />
-        <p className={styles.error}>Error</p>
+        <input
+          type={type}
+          id={name}
+          name={name}
+          className={styles.input}
+          onChange={onChange}
+          value={value}
+          onBlur={onBlur}
+        />
+
+        {error && <p className={styles.error}>Error</p>}
       </label>
     </div>
   );
